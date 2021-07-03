d3f4c5b07c4e7eb9b02c48e9776a2876c49da7ec
joaohqueiroz <johnrick.jh@gmail.com>
2021-07-03 19:55:31 -0300

    Configurando EJS

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', async (req, res) => {
+    res.render('index');
+});
+
+app.listen(3000);
